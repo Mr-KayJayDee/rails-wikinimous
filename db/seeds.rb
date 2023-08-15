167a5866923d2d43c39188e06ce669c1383e88f5
Killian' DAL-CIN <killian.dalcin@gmail.com>
2023-08-15 13:36:18 +0200

    feat(wikinomous): Create wikinomous
    The exercise is finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+# Create 10 posts with a title and content using Faker
+
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    content: Faker::Lorem.paragraph(sentence_count: 2)
+  )
+end
+puts "10 posts have been created"
